6dea7504131e6b4dab0a0f91f1362d7fb09d64b0
PLHuyen <huyen.phan@onemount.com>
2021-08-31 09:46:31 +0700

    Initial commit

---
+++ src/main/java/Test3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Nhập tháng = ");
+        int n = scan.nextInt();
+
+        int day, month;
+        if (n > 0 && n <13) {
+            if (n == 1 || n == 3 || n == 5 || n == 7 || n == 8 || n == 10 || n == 12) {
+                System.out.format("Tháng %d có 31 ngày\n", n);
+            }
+            if (n == 2) {
+                System.out.println("Tháng %d có 28 hoặc 29 ngày\n");
+            }
+            if (n == 4 || n == 6 || n == 9 || n == 11) {
+                System.out.format("Tháng %d có 30 ngày\n", n);
+            }
+        }
+        else
+            System.out.println("Tháng vừa nhập không hợp lệ\n");
+    }
+}
